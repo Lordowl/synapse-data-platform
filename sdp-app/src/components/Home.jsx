cb728d0cebd4af91f3d5e370bf95f5df571a686c
Emanuele De Feo <emanuele.defeo@engagegroup.it>
2025-10-02 12:54:05 +0200

    chore: update version number to 0.2.2 in Home component

--- sdp-app/src/components/Home.jsx
+++ sdp-app/src/components/Home.jsx
@@ -142,7 +142,7 @@ function Home({ setIsAuthenticated }) {
       </div>
 
       <div className="version-footer">
-        <small style={{color: '#666', fontSize: '12px'}}>Versione 0.2.1 - Patch di test autoupdate</small>
+        <small style={{color: '#666', fontSize: '12px'}}>Versione 0.2.2 - Patch di test autoupdate</small>
       </div>
     </div>
   );
